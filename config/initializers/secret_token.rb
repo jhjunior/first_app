5e3825d2ded4ad801e26e3d618a940f2973257e0
jjunior <webmedia@gmail.com>
2012-03-19 19:53:22 -0300

    Trocando para o postgresql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'd06d52ec3bdaf74d8f6b96e6ebb84153362e80a1bfd8c7a2638dfe70c9f219ac14e912f8895746e38aed1ab019a45b1b3a2c801f51a58a5f305cc5cc137551b5'
+FirstApp::Application.config.secret_token = '9705df1b0217d9594bdaf7952cc0f32f85786ab6a5ce54a316d1ad70a2264d34a9e2558fd46893dd7568106eb98cf8ee09e9cc463ebc9e6ade9cc663b923637a'
